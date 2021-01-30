3863154683dc570e66223c20b428d0dbcf8ed04c
condor233 <e.hdelemos@gmail.com>
2021-01-30 13:35:33 -0300

    Extra o valor dos botoes

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -7,6 +7,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora")
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -20,8 +21,22 @@ class CalcController {
 
     }
 
+    initButtonsEvents() {
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e => {
+                console.log(btn.className.baseVal.replace('btn-', ''))
+            })
+        })
+    }
+
     setDisplayDateTime() {
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month: "short",
+            year: "numeric"
+        })
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
     }
 
